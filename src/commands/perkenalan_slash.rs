ac50d622dba8fcf4ac2cbe6986ec8882dda391d5
Yunichie <alfarelsandriano@gmail.com>
2022-09-22 22:26:11 +0700

    Initial commit

---
+++ src/commands/perkenalan_slash.rs
@@ -0,0 +1,57 @@
+use serenity::{
+    builder::{
+        CreateApplicationCommand
+    },
+    model::{
+        prelude::{
+            command::{
+                CommandOptionType
+            },
+            interaction::{
+                application_command::{
+                    CommandDataOption,
+                    CommandDataOptionValue
+                }
+            }
+        }
+    }
+};
+
+pub fn register(
+    command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
+    command
+        .name("perkenalan")
+        .description("Perkenalkan diri kamu!")
+        .create_option(|option| {
+            option
+                .name("nama")
+                .description("Masukkan nama kamu!")
+                .kind(CommandOptionType::String)
+                .required(true)
+        })
+        .create_option(|option| {
+            option
+                .name("kelas")
+                .description("Kelas berapa kamu?")
+                .kind(CommandOptionType::String)
+                .required(true)
+        })
+        .create_option(|option| {
+            option
+                .name("angkatan")
+                .description("Angkatan tahun berapa kamu?")
+                .kind(CommandOptionType::String)
+                .required(true)
+        })
+}
+
+pub fn run(options: &[CommandDataOption]) -> String {
+    let option = options
+        .get(0)
+        .expect("Expected string option")
+        .resolved
+        .as_ref()
+        .expect("Expected string object");
+    
+        // then what?
+}
