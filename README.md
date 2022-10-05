661e64f65e2ce213df7bd0318a62c1fd767dc9af
Yunichie <alfarelsandriano@gmail.com>
2022-10-06 01:14:38 +0700

    [UNFINISHED], 5, task update; kapitalisasi

--- README.md
+++ README.md
@@ -2,11 +2,18 @@
 
 ## Bantuan
 - [ ] Application; client; bot avatar.
+- [ ] Button (bantuan dibutuhkan untuk perkenalan menggunakan Modal).
 
 ## Perkenalan-slash
 - [ ] Gambar "selamat datang".
 - [ ] Batasan:
-   - [ ] Pemilik role `smasaku` tidak bisa menggunakan perintah.
-   - [ ] Perintah hanya bisa dijalankan di kanal `#introduction`.
+  - [ ] Pemilik role `smasaku` tidak bisa menggunakan perintah.
+  - [ ] Perintah hanya bisa dijalankan di kanal `#introduction`.
 - [ ] Field, dan opsi untuk `Media Sosial`.
-- [ ] Batasan (Regular Expression) untuk `Nama`, `Kelas`, `Angkatan`, dan `Media Sosial`.
+  - [ ] Field.
+  - [X] Opsi.
+- [ ] Batasan (Regular Expression) untuk `Kelas`, dan `Angkatan`.
+- [ ] Kapitalisasi.
+
+## Extra Crate
+- [ ] capitalize() sebagai "crate" tersendiri.
--- src/commands/perkenalan_slash.rs
+++ src/commands/perkenalan_slash.rs
@@ -22,6 +22,7 @@ use serenity::{
         *
     }
 };
+use regex::Regex;
 
 pub fn register(
     command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
@@ -87,12 +88,15 @@ pub async fn run(options: &ApplicationCommandInteraction, ctx: &Context) {
     .find(|option| option.name == "medsos")
     .cloned();
 
+    let nama = nama.unwrap().value.unwrap().as_str().unwrap().to_owned();
+    let kelas = kelas.unwrap().value.unwrap().as_str().unwrap().to_owned();
+    let angkatan = angkatan.unwrap().value.unwrap().as_str().unwrap().to_owned();
+
     println!("{:?}", medsos);
-    /*match medsos {
-        None => {
-            medsos = medsos.unwrap().value.unwrap().as_str().unwrap() = "-";
-            medsos = "-";
-        }
+    
+    // TODO: medsos
+    /*if medsos.is_none() {
+        medsos = "-";
     }*/
 
     //let medsos = medsos.unwrap().value.unwrap();
@@ -102,19 +106,32 @@ pub async fn run(options: &ApplicationCommandInteraction, ctx: &Context) {
     //let cache = &ctx.cache;
     let roles = &options.member.as_ref().unwrap().roles;
 
-    // Ganti role id!
+    // TODO: role id & channel id
     // User sudah mempunyai role smasaku
     if roles.iter().any(|&i| i.as_u64() == &1025826518259224608) {
         println!("contains 1025826518259224608");
     }
 
     // User belum mempunyai role smasaku
-    if roles.iter().any(|&i| i.as_u64() == &1025826518259224608) == false {
-        println!("contains 1025826518259224608");
+    if !roles.iter().any(|&i| i.as_u64() == &1025826518259224608) {
+        println!("does not contain 1025826518259224608");
     }
 
+    // TODO: RegEx; embed error
     // RegEx
+    let xi_xii = Regex::new(r"^(XI|XII)\s(MIPA|IPS)\s([0-9]|1[0-2])$").unwrap();
+    let x = Regex::new(r"^(X)\s([0-9]|1[0-2])$").unwrap();
+    let regex_angkatan = Regex::new(r#"^(([0-9]){4})|(^([0-9]){4}/([0-9]){4})$"#).unwrap();
 
+    /*if !(x.is_match(&kelas)) && !(xi_xii.is_match(&kelas)) {
+        // Kirim embed error!
+        todo!();
+    }
+
+    if !(regex_angkatan.is_match(&angkatan)) {
+        // Kirim embed error!
+        todo!();
+    }*/
 
     let perkenalan_slash = options
     .create_interaction_response(&ctx.http, |resp| {
@@ -122,26 +139,55 @@ pub async fn run(options: &ApplicationCommandInteraction, ctx: &Context) {
         .kind(InteractionResponseType::ChannelMessageWithSource)
         .interaction_response_data(|msg| {
             msg
-            //.add_file("./image/welcome.jpg")
             .embed(|e| {
                 e
                 .color((247, 10, 10))
                 .title("Perkenalan")
                 .fields(vec![
-                    ("Nama", nama.unwrap().value.unwrap().as_str().unwrap(), false),
-                    ("Kelas", kelas.unwrap().value.unwrap().as_str().unwrap(), false),
-                    ("Angkatan", angkatan.unwrap().value.unwrap().as_str().unwrap(), false),
+                    ("Nama", capitalize(&nama), false),
+                    ("Kelas", kelas.to_uppercase(), false),
+                    ("Angkatan", angkatan, false),
                     //("Media Sosial", medsos, false)
                 ])
                 .thumbnail(&user.avatar_url().unwrap())
                 .footer(|f| f.text(&user.tag()))
                 .timestamp(Timestamp::now())
             })
+            // TODO: .add_file()
             //.add_file("./image/welcome.jpg")
-        })
-    }).await;
+        })//.edit_original_interaction_response()
+    })
+    .await;
 
     if let Err(why) = perkenalan_slash {
         println!("Error sending message: {:?}", why);
     }
+}
+
+// TODO: Kapitalisasi; pindahkan ke file tersendiri untuk pemakaian di luar perintah `perkenalan_slash`(?)
+fn capitalize(string: &str) -> String {
+    let s = string.to_lowercase();  // lowercase to make the string uniform
+    let s_bind = &s.split_whitespace().collect::<Vec<&str>>();  // split string at whitespace, store into an array
+    let find_first_letter = Regex::new(r"\b(\w)").unwrap(); // "take every character after the word boundary"
+    let mut return_this = String::new();    // an empty string for storing away the modified string
+
+    for i in s_bind {   // iterate through array (s_bind; splitted at whitespace strings)
+        return_this.push_str(
+            &find_first_letter
+            .replace(i, 
+                i
+                .chars()
+                .next()
+                .unwrap()
+                .to_uppercase()
+                .to_string())
+            .to_string()
+            .chars()
+            .collect::<String>()
+        );  // push the matched and uppercased character (replaced) to the empty string, join together with the unmodified string
+        return_this.push_str(" ");  // join a whitespace
+    }
+    return_this.pop();  // because of the loop, there will be an extra whitespace at the end of the string. Delete it.
+
+    return return_this  // this should now capitalize the first letter of each given set of string. Correctly.
 }
